22679552bcd588bf9c24be975fdd3321d99cea66
Wesley Alves de Albuquerque <wealbuquerque@yahoo.com.br>
2023-03-08 17:44:33 -0300

    Adicionado Model conta na function

---
+++ Grupo6.Logistics.WarehouseFunction/Model/Conta.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo6.Logistics.WarehouseFunction.Model
+{
+    public class Conta
+    {
+        public Entity Account = new Entity("account");
+        public string LogicalName { get; set; }
+        public string CNPJ { get; set; }
+        public string Telefone { get; set; }
+        public string Fax { get; set; }
+        public string Site { get; set; }
+        public string CNPJ_Conta { get; set; }
+        public string SimboloAcao { get; set; }
+        public string DataUltVisita { get; set; }
+        public Endereco Endereco { get; set; }
+        public string CPF_Contato { get; set; }
+        public string Descricao { get; set; }
+        
+        public Conta 
+
+    }
+}
