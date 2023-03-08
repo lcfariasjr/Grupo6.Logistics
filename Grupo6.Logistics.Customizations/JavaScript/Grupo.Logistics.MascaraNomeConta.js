4c41b216faf630a39b977764162fce0e1d638a95
lcfariasjr <lcfariasjr@hotmail.com>
2023-03-08 20:20:16 -0300

    Mascara Nome

--- Grupo6.Logistics.Customizations/JavaScript/Grupo.Logistics.MascaraNomeConta.js
+++ Grupo6.Logistics.Customizations/JavaScript/Grupo.Logistics.MascaraNomeConta.js
@@ -1,1 +1,43 @@
-// JavaScript source code
+if (typeof (LogisticsPrincipal) == "undefined") { LogisticsPrincipal = {} }
+if (typeof (LogisticsPrincipal.Conta) == "undefined") { LogisticsPrincipal.Conta = {} }
+
+
+LogisticsPrincipal.Conta = {
+    OnCreateConta: function (executionContext) {
+        var formContext = executionContext.getFormContext();
+        var nomeConta = formContext.getAttribute("name").getValue().toLowerCase();
+        var formatedName = nomeConta.split(" ");
+
+        if (nomeConta != null) {
+
+            for (var i = 0; i < formatedName.length; i++) {
+                var n = formatedName[i];
+
+                var firstLetter = n[0];
+
+                if (n.length > 2) {
+                    n = firstLetter.toUpperCase() + n.slice(1);
+                }
+                else {
+                    n = firstLetter + n.slice(1);
+                }
+
+                formatedName[i] = n;
+
+            }
+
+            nomeConta = formatedName.join(" ");
+        }
+        else {
+            return;
+        }
+
+        formContext.getAttribute("name").setValue(nomeConta);
+        return formatedName;
+
+    }
+
+
+
+}
+
