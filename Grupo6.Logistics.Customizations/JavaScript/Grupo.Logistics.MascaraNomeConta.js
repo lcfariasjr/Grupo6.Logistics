2ad4b09b58804f172e792165a3c632bdae842ee9
Wesley Alves de Albuquerque <wealbuquerque@yahoo.com.br>
2023-03-10 22:31:42 -0300

    Ajustado JS de MascaraConta

--- Grupo6.Logistics.Customizations/JavaScript/Grupo.Logistics.MascaraNomeConta.js
+++ Grupo6.Logistics.Customizations/JavaScript/Grupo.Logistics.MascaraNomeConta.js
@@ -5,35 +5,39 @@ if (typeof (LogisticsPrincipal.Conta) == "undefined") { LogisticsPrincipal.Conta
 LogisticsPrincipal.Conta = {
     OnCreateConta: function (executionContext) {
         var formContext = executionContext.getFormContext();
-        var nomeConta = formContext.getAttribute("name").getValue().toLowerCase();
+        var nomeConta = formContext.getAttribute("name").getValue();
+        if (nomeConta != null & nomeConta != "") {
+            nomeConta = nomeConta.toLowerCase();
         nomeConta = nomeConta.replace(/\s\s+/g, ' ');
         var formatedName = nomeConta.split(" ");
 
-        if (nomeConta != null) {
+            if (nomeConta != null) {
 
-            for (var i = 0; i < formatedName.length; i++) {
-                var n = formatedName[i];
+                for (var i = 0; i < formatedName.length; i++) {
+                    var n = formatedName[i];
 
-                var firstLetter = n[0];
+                    var firstLetter = n[0];
 
-                if (n.length > 2) {
-                    n = firstLetter.toUpperCase() + n.slice(1);
+                    if (n.length > 2) {
+                        n = firstLetter.toUpperCase() + n.slice(1);
+                    }
+                    else {
+                        n = firstLetter + n.slice(1);
+                    }
+
+                    formatedName[i] = n;
+
                 }
-                else {
-                    n = firstLetter + n.slice(1);
-                }
 
-                formatedName[i] = n;
+                nomeConta = formatedName.join(" ");
+                formContext.getAttribute("name").setValue(nomeConta);
 
             }
-
-            nomeConta = formatedName.join(" ");
         }
         else {
             return;
         }
 
-        formContext.getAttribute("name").setValue(nomeConta);
     }
 
 
